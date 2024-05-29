dc5edf5fa7606c63d343d7931de058adb6441b09
Olga Adams <83266819+Olga975@users.noreply.github.com>
2024-05-29 11:34:11 -0600

    BrowserRouter, Routes, and Route (#11)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
@@ -10,9 +11,7 @@ function App() {
   //getting my tasks from airtable
 
   const fetchData = async () => {
-    const url = `https://api.airtable.com/v0/${
-      import.meta.env.VITE_AIRTABLE_BASE_ID
-    }/${import.meta.env.VITE_TABLE_NAME}`
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
     const options = {
       method: "GET",
       headers: {
@@ -25,23 +24,20 @@ function App() {
         throw new Error(`Error: ${response.status}`);
       }
 
-      const data = await response.json()
+      const data = await response.json();
 
-      const todos = data.records.map((todo) => {
-        const newTodo = {
-          id: todo.id,
-          title: todo.fields.title,
-        };
-        return newTodo
-      });
-      setTodoList(todos)
-      setIsLoading(false)
+      const todos = data.records.map((todo) => ({
+        id: todo.id,
+        title: todo.fields.title,
+      }));
+      setTodoList(todos);
+      setIsLoading(false);
     } catch (error) {
-      console.log(`Error: ${response.status}`)
+      console.log(`Error: ${error.message}`);
     }
   };
 
-  //post task 
+  //post task
   const postTodo = async (todo) => {
     try {
       const airtableData = {
@@ -50,9 +46,7 @@ function App() {
         },
       };
       const response = await fetch(
-        `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${
-          import.meta.env.VITE_TABLE_NAME
-        }`,
+        `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`,
         {
           method: "POST",
           headers: {
@@ -63,25 +57,23 @@ function App() {
         }
       );
       if (!response.ok) {
-        throw new Error(`Error: ${response.status}`)
+        throw new Error(`Error: ${response.status}`);
       }
 
-      const dataResponse = await response.json()
-      console.log(dataResponse)
-      return { id: dataResponse.id, title: dataResponse.fields.title }
+      const dataResponse = await response.json();
+      console.log(dataResponse);
+      return { id: dataResponse.id, title: dataResponse.fields.title };
     } catch (error) {
-      console.log(`Error: ${response.status}`)
+      console.error(`Error: ${error.message}`);
     }
-  }
+  };
 
-  //delete the task 
+  //delete the task
 
   const postDeleteTodo = async (id) => {
     try {
       const response = await fetch(
-        `https://api.airtable.com/v0/${
-          import.meta.env.VITE_AIRTABLE_BASE_ID
-        }/Default${id}`,
+        `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/Default${id}`,
         {
           method: "DELETE",
           headers: {
@@ -91,50 +83,61 @@ function App() {
         }
       );
       if (!response.ok) {
-        throw new Error(`Error: ${response.status}`)
+        throw new Error(`Error: ${response.status}`);
       }
       const data = await response.json();
-      return data
+      return data;
     } catch (error) {
-      console.log(error.message)
+      console.log(`Error: ${error.message}`);
     }
   };
 
   useEffect(() => {
-    fetchData()
-  }, [fetchData])
-
+    fetchData();
+  }, []);
 
   const addTodo = async (todo) => {
     try {
-      const data = await postTodo(todo)
-      setTodoList([...todoList, data])
+      const data = await postTodo(todo);
+      setTodoList([...todoList, data]);
     } catch (error) {
-      console.error(error.message);
+      console.error(`Error: ${error.message}`);
     }
   };
 
   const removeTodo = async (id) => {
     try {
-      await postDeleteTodo(id)
-      const updatedTodoList = todoList.filter((todo) => todo.id !== id)
-      setTodoList(updatedTodoList)
+      await postDeleteTodo(id);
+      const updatedTodoList = todoList.filter((todo) => todo.id !== id);
+      setTodoList(updatedTodoList);
     } catch (error) {
-      console.error(error.message)
+      console.error(`Error: ${error.message}`);
     }
-  }
+  };
 
-  return (
-    <>
-      <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={addTodo} />
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
-      )}
-    </>
-  )
+
+return (
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <h1>Todo List</h1>
+            <AddTodoForm onAddTodo={addTodo} />
+            {isLoading ? (
+              <p>Loading...</p>
+            ) : (
+              <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+            )}
+          </>
+        }
+      />
+      <Route path="/new" element={<h1>New Todo List</h1>} />
+    </Routes>
+  </BrowserRouter>
+)
+
 }
 
 export default App;
