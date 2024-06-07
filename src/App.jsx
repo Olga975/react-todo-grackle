8eaa6bef7ec5561782f626e6d84433c29357924c
Olga Adams <helga7777@gmail.com>
2024-06-06 23:52:09 -0600

    css changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
+
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -115,29 +116,29 @@ function App() {
     }
   };
 
-
-return (
-  <BrowserRouter>
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <>
-            <h1>Todo List</h1>
-            <AddTodoForm onAddTodo={addTodo} />
-            {isLoading ? (
-              <p>Loading...</p>
-            ) : (
-              <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
-            )}
-          </>
-        }
-      />
-      <Route path="/new" element={<h1>New Todo List</h1>} />
-    </Routes>
-  </BrowserRouter>
-)
-
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <h1>Todo List</h1>
+                <AddTodoForm onAddTodo={addTodo} />
+                {isLoading ? (
+                  <p>Loading...</p>
+                ) : (
+                  <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+                )}
+              </>
+            }
+          />
+          <Route path="/new" element={<h1>New Todo List</h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
